4c5c41771de6f86b15f7f0d2490acc1cf67cccf7
Blagovest-Todorov <blagovest.stod@abv.bg>
2020-11-18 15:26:17 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace copyingArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] gosho = {5}; //array of intergers
+            int[] georgy = (int[])gosho.Clone(); //Clones the above array but result is in Object type, we convert//
+            //object type to 
+            
+
+        }
+        
+    }
+}
